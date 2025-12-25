b1b5b994c28c2a2bd471315767e97b49dc850852
PCfVW <ejacopin@ymail.com>
2025-12-25 21:30:44 +0100

    chore(typescript): update @version tags to 2.1.2
    - Update @version in PriorityQueue.ts from 2.0.0 to 2.1.2
    - Update @version in comparators.ts from 2.0.0 to 2.1.2
    - Update @version in index.ts from 2.0.0 to 2.1.2
    - Maintain consistent version documentation across TypeScript source files

--- TypeScript/src/comparators.ts
+++ TypeScript/src/comparators.ts
@@ -2,7 +2,7 @@
  * Pre-built comparator factories for common use cases.
  *
  * @module comparators
- * @version 2.0.0
+ * @version 2.1.2
  * @license Apache-2.0
  */
 
--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -5,7 +5,7 @@
  *
  * @packageDocumentation
  * @module d-ary-heap
- * @version 2.0.0
+ * @version 2.1.2
  * @license Apache-2.0
  * @copyright 2023-2025 Eric Jacopin
  */
